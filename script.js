7d5f506eb4520e4d0496b497e0a4649cc98147a8
CocaDoYou <99327163+CocaDoYou@users.noreply.github.com>
2022-02-09 13:23:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let game= document.getElementById("games")
+let snake= document.getElementById("snake")
+let gir= document.getElementById("girlyandy")
+snake.onclick = function(){
+    game.setAttribute("src","pages/Snake/index.html")
+}
+gir.onclick = function(){
+    game.setAttribute("src","pages/girlyandy/js 9.html")
+}
